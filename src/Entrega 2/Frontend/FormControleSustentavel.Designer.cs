43f860d206ca3647410aeda9b9d225ceafe9f13e
DandaraDandara <140045747+DandaraDandara@users.noreply.github.com>
2025-05-18 23:33:40 -0300

    FormsControleSustentavel

---
+++ src/Entrega 2/Frontend/FormControleSustentavel.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace SmartHomeApp
+{
+    partial class FormControleSustentavel
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button btnVerSimulacao;
+        private Button btnHome;
+
+        private void InitializeComponent()
+        {
+            btnVerSimulacao = new Button();
+            btnHome = new Button();
+            SuspendLayout();
+            // 
+            // btnVerSimulacao
+            // 
+            btnVerSimulacao.Location = new Point(80, 50);
+            btnVerSimulacao.Name = "btnVerSimulacao";
+            btnVerSimulacao.Size = new Size(200, 40);
+            btnVerSimulacao.TabIndex = 0;
+            btnVerSimulacao.Text = "Simular Dados Sustentáveis";
+            btnVerSimulacao.Click += btnVerSimulacao_Click;
+            // 
+            // btnHome
+            // 
+            btnHome.Location = new Point(140, 120);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(75, 30);
+            btnHome.TabIndex = 1;
+            btnHome.Text = "🏠";
+            btnHome.Click += btnHome_Click;
+            // 
+            // FormControleSustentavel
+            // 
+            ClientSize = new Size(360, 200);
+            Controls.Add(btnVerSimulacao);
+            Controls.Add(btnHome);
+            Name = "FormControleSustentavel";
+            Text = "Controle Sustentável";
+            Load += FormControleSustentavel_Load;
+            ResumeLayout(false);
+        }
+    }
+}
---
+++ src/Entrega 2/Frontend/FormControleSustentavel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SmartHomeApp
+{
+    public partial class FormControleSustentavel : Form
+    {
+        public FormControleSustentavel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerSimulacao_Click(object sender, EventArgs e)
+        {
+            FormSimulacao simulacao = new FormSimulacao();
+            simulacao.Show();
+            this.Hide();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            FormUsuario home = new FormUsuario();
+            home.Show();
+            this.Hide();
+        }
+
+        private void FormControleSustentavel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
